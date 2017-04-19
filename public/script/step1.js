7181495836809c7174b843380eba0b7d6df561cd
Crystal Tate <crys.tate@icloud.com>
2017-04-19 07:51:10 -0500

    adds groups dropdown

--- public/script/step1.js
+++ public/script/step1.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
     event.preventDefault()
     $('#group-save').toggle()
   })
+
+  $('#group-selector').click(function(event) {
+    event.preventDefault()
+    $('#group-menu').toggle()
+  })
+
+  $('#group-menu a').click(function(event) {
+    event.preventDefault()
+    $('#group-selector').text($(this).text())
+    $('#group-menu').hide()
+    $('#group-menu a').removeClass('selected')
+    $(this).addClass('selected')
+  })
 })
--- routes/midpoint.js
+++ routes/midpoint.js
@@ -7,8 +7,18 @@ require('dotenv').load();
 
 router.route('/')
   .get((req, res) => {
-    console.log(req.session);
-    res.render('statics/map', {apiKey: process.env.API_KEY});
+    knex('users_groups')
+      .leftJoin('groups', 'users_groups.group_id', 'groups.id')
+      .where({
+        user_id: req.currentUser.id
+      })
+      .then((groups)=> {
+        res.render('statics/map', {
+          apiKey: process.env.API_KEY,
+          groups: groups
+        });
+      })
   });
+
 
 module.exports = router;
